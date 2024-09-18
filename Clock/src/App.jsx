a884e72ab6be59147d3140f02987e02b1a9a5830
shubham chauhan <shubhamchauhan@shubhams-MacBook-Air.local>
2024-09-18 18:15:54 +0530

    First-commit

---
+++ Clock/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DigitalClock from './DigitalClock'
+
+const App = () => {
+  return (
+    <div className='main'>
+      
+      <DigitalClock />
+    </div>
+  )
+}
+
+export default App
